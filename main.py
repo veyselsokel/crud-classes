d1cab511ee4bf391a15bf1a730a0ed1025e920a7
Veysel Sökel <73946954+veyselsokel@users.noreply.github.com>
2024-01-14 00:47:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pymysql.cursors
-
 import firebaseCRUD
 from MySqlCrud import MySQLCRUD
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     database = input("Hangi database ile çalışmak istiyorsun 1:Firebase 2:MySQL")
     if database == "1":
@@ -27,10 +21,8 @@ if __name__ == '__main__':
     elif database =="2":
         mysql_crud = MySQLCRUD()
         mysql_crud.veri_ekle( {"isim": "Ahmet", "yas": 25})
-        #print(mysql_crud.get_last_insert_id())
         last_insert_id=mysql_crud.get_last_insert_id()
         print(mysql_crud.veri_oku(last_insert_id))
-
         mysql_crud.veri_guncelle(last_insert_id, {"isim": "Mehmet", "yas": 26})
         print(mysql_crud.veri_oku(last_insert_id))
         mysql_crud.veri_sil(last_insert_id)
